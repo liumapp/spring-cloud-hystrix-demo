ed9b83788567a743981a61d2687cdb11738a693c
liumapp <liumapp.com@gmail.com>
2018-01-27 09:06:15 +0800

    add sleep time for client

--- hystrix-demo-client-b/src/main/java/com/liumapp/demo/hystrix/client/b/controller/IndexController.java
+++ hystrix-demo-client-b/src/main/java/com/liumapp/demo/hystrix/client/b/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.liumapp.demo.hystrix.client.b.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Created by liumapp on 9/28/17.
@@ -14,8 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private DiscoveryClient client;
+
     @RequestMapping("/")
-    public String index (ModelMap model) {
+    public String index (ModelMap model) throws InterruptedException {
+        ServiceInstance instance = client.getLocalServiceInstance();
+
+        int sleepTime = new Random().nextInt(3000);
+        System.out.println("sleepTime is : " + sleepTime);
+        Thread.sleep(sleepTime);
+
         return "hello , this is client b demo";
     }
 
